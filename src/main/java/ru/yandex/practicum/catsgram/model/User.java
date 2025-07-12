ffe649ef3905a10d84f0f77a19a2a0a2b1dd2f5e
Maria Pokasova <mpokasova99@gmail.com>
2025-07-12 18:31:16 +0300

    init

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+import java.time.Instant;
+
+@Data
+public class User {
+    Long id;
+    String username;
+    String email;
+    String password;
+    Instant registrationDate;
+}
