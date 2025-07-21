e8ac44d3136b474beea915e180020b504db0ec46
Maria Pokasova <mpokasova99@gmail.com>
2025-07-21 08:41:48 +0300

    commit

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.catsgram.exception.ParameterNotValidException;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.model.SortOrder;
 import ru.yandex.practicum.catsgram.service.PostService;
@@ -22,7 +23,17 @@ public class PostController {
     public Collection<Post> findAll(@RequestParam(defaultValue = "desc") String sort,
                                     @RequestParam(defaultValue = "0") int from,
                                     @RequestParam(defaultValue = "10") int size) {
-        return postService.findAll(SortOrder.from(sort), from, size);
+        SortOrder order = SortOrder.from(sort);
+        if (order == null) {
+            throw new ParameterNotValidException("sort", "Получено: " + sort + " должно быть: ask или desc");
+        }
+        if (from < 0) {
+            throw new ParameterNotValidException("from", "Начало выборки должно быть положительным числом");
+        }
+        if (size <= 0) {
+            throw new ParameterNotValidException("size", "Некорректный размер выборки. Размер должен быть больше нуля");
+        }
+        return postService.findAll(order, from, size);
     }
 
     @GetMapping("/{postId}")
---
+++ src/main/java/ru/yandex/practicum/catsgram/exception/ParameterNotValidException.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.catsgram.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ParameterNotValidException extends IllegalArgumentException {
+    private String parameter;
+    private String reason;
+
+    public ParameterNotValidException(String parameter, String reason) {
+        this.parameter = parameter;
+        this.reason = reason;
+    }
+}
