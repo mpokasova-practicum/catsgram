f5a6441daf5918965a343b5c2923d0d863336a05
Maria Pokasova <mpokasova99@gmail.com>
2025-08-12 21:13:32 +0300

    commit jdbc

--- src/main/java/ru/yandex/practicum/catsgram/model/User.java
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -1,13 +1,16 @@
 package ru.yandex.practicum.catsgram.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.time.Instant;
 
 @Data
+@EqualsAndHashCode(of = { "email" })
 public class User {
-    Long id;
-    String username;
-    String email;
-    String password;
-    Instant registrationDate;
+    private long id;
+    private String username;
+    private String email;
+    private String password;
+    private Instant registrationDate;
 }
--- src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.catsgram.service;
 
 import lombok.RequiredArgsConstructor;
-//import org.springframework.beans.factory.annotation.Value;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
